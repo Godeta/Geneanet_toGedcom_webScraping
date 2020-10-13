4d93da659a90d48f3bc161cdac55c79511dae3d2
Jean erick Lemoine <jujulem@sfr.fr>
2020-10-13 16:03:19 +0200

    fonction de verif et sosa quand existant

--- webScraping/fonctionsScrap.php
+++ webScraping/fonctionsScrap.php
@@ -3,11 +3,31 @@
 include('advanced_html_dom.php');	
 
 function scrapPrenom($html){
-    $data=$html->find('#person-title > div > h1 > a:nth-child(2)','0');
+    $data=$html->find('#person-title > div > h1 > a:nth-child(2)','0')->innertext;
     return $data ;
 }
 
 function scrapNom($html){
-    $data=$html->find('#person-title > div > h1 > a:nth-child(3)','0');
+    $data=$html->find('#person-title > div > h1 > a:nth-child(3)','0')->innertext;
     return $data ;
+}
+
+function scrapSosa($html){
+    $data=$html->find('.sosa > a','0');
+    if(existe($data)){
+        return $data->innertext;
+    }
+    else{
+        return $data;
+    }
+    return $data;
+}
+
+function existe($data){
+    if (empty($data)){
+        return false;
+    }
+    else{
+        return true;
+    }
 }
--- webScraping/test.php
+++ webScraping/test.php
@@ -1,10 +1,14 @@
 <?php
 include 'fonctionsScrap.php';
 
-$html='https://gw.geneanet.org/gbernard6?n=goduet&oc=&p=marie';
-$html=file_get_html($html);
-$test=scrapNom($html);
-echo $test->innertext;
+//$html='https://gw.geneanet.org/scalpa49?n=lemoine&oc=&p=nicolas';
+$html = 'https://gw.geneanet.org/micolon1?n=lemoine&oc=&p=paul+emile+prosper';
+$html = file_get_html($html);
+$test = scrapNom($html);
+echo $test;
 echo "</br>";
-$test=scrapPrenom($html);
-echo $test->innertext;
+$test = scrapPrenom($html);
+echo $test;
+echo "</br>";
+$test = scrapSosa($html);
+echo $test;
