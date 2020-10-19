b37a4ca9ddd56979bb41db749bcfdd4c25409256
Godeta <Godeta61@gmail.com>
2020-10-19 21:16:36 +0200

    changement méthode de transmission url, du GUI au scraping PHP, ecriture dans un fichier

--- Desktop_GUI/simpleGUI.py
+++ Desktop_GUI/simpleGUI.py
@@ -3,19 +3,17 @@ import tkinter as tk
 import os 
 url = ""
 
-def launchPHP(url):
-    """Fonction permettant de lancer le fichier test.php en ligne de commande (nécessite d'avoir php dans le path) """
-    table = [url[i:i+40] for i in range(0, len(url), 40)]
-    # on sépare l'url tous les 40 caractères en plusieurs arguments car depuis le cmd un argument est limité à 43 chars
-    commande = 'cd webScraping && php execScrap.php '
-    for i in range(len(table)):
-        commande += ' '+table[i]
-        print(table[i])
+def launchPHP(url,choice = "all"):
+    "Fonction permettant de lancer le fichier test.php en ligne de commande (nécessite d'avoir php dans le path) "
+    f = open("url.txt", "w")
+    f.write(url)
+    f.close()
+    commande = 'cd webScraping && php execScrap.php '+choice
     os.system(commande)
-    print (url)
+    print (url+ "\n"+choice)
 
 def createWindow():
-    """Fonction principale contenant la création de la fenêtre et ses intéractions """
+    "Fonction principale contenant la création de la fenêtre et ses intéractions "
     window = tk.Tk()
     greeting = tk.Label(text="Bienvenu, entrez l'url d'un profil pour lequel vous soihaitez récupérer les données.",
         foreground="blue"  # Set the text color to white
--- webScraping/execScrap.php
+++ webScraping/execScrap.php
@@ -1,19 +1,25 @@
 <?php
 include 'fonctionsScrap.php';
 
+function recupURL(){
+    $monfichier = fopen('../url.txt', 'r');
+    $first_line = fgets($monfichier); 
+    return $first_line ;
+}
+
 // scrapping total d'une page appelé par l'interface graphique et gestion des exceptions
 var_dump($argv); //récupération d'un tableau d'arguments
-$url = 'https://gw.geneanet.org/gstutz?n=le+conquerant&oc=&p=guillaume';
+$url = recupURL();
+$choice = "";
 if(!empty($argv[1])) {
-    $url = "";
-    for ($i =1; $i<count($argv);$i++) {
-$url = $url.$argv[$i];
-    }
-    $url = str_replace(' ', '', $url);
-    echo "\nL'url : ".$url;
+$choice = $argv[1];
+}
+if(empty($url)) {
+    $url = 'https://gw.geneanet.org/gstutz?n=le+conquerant&oc=&p=guillaume';
 }
 echo ("\n Full scrapping :\n");
 try {
+echo ("\n L'URL : ".$url);
 $html = @file_get_html($url);
 }
 catch (Exception $e) {
